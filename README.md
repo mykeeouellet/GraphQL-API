c3327a244cf5c562357c7d1fac9c417143d00b08
agivizvari <agivizvari@gmail.com>
2020-04-01 10:34:35 -0400

    First commit

--- README.md
+++ README.md
@@ -3,5 +3,15 @@ GraphQL API // Codeboxx Week-8
 
 This project is using Node.js and Express to create a GraphQL Api.
 
+Answer to the 3 questions:
+
+1. Retrieving the address of the building, the beginning and the end of the intervention for a specific intervention.
+
+2. Retrieving customer information and the list of interventions that took place for a specific building.
+
+3. Retrieval of all interventions carried out by a specified employee with the buildings associated with these interventions including the details (Table BuildingDetails) associated with these buildings.
+
+
+
 
 
--- server.js
+++ server.js
@@ -3,18 +3,47 @@ var express_graphql = require('express-graphql');
 var {buildSchema} = require('graphql');
 
 // GraphQL Schema
+// type Query: special root type, entry points for the request sent by client
 var schema = buildSchema(`
     type Query {
-        message: String
+        intervention(id: Int!): Intervention
+        building(id: Int!): Building
+        customer(id: Int!): Customer
+        employee(id: Int!): Employee
+        building_detail(id: Int!): Building_detail       
     }
+
+    type Intervention{
+        id: Int!
+        start_date_time_intervention: String
+        end_date_time_intervention: String
+    }
+    type Building{
+        id: Int!
+        building_detail: [Building_detail]
+
+    }
+    type Customer{
+        id: Int!
+    }
+    type Employee{
+        id: Int!
+    }
+    type Building_detail{
+        id: Int!
+    }
+    type Address{}
 `);
 
-// Root Resolver
+Root Resolver
 var root = {
-    message: () => 'Hello World!'
+    intervention: testing
+
 };
 
-// Create an express server and a GraphQL endpoint
+function testing()
+
+Create an express server and a GraphQL endpoint
 var app = express();
 app.use('/graphql', express_graphql({
     schema: schema,
