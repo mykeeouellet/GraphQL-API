ea825c08e0111deca9de9eb1a3e3137fc58eaeea
mykeeouellet <mykee.o@icloud.com>
2020-04-01 10:30:12 -0700

    added mysql connection

--- server.js
+++ server.js
@@ -1,17 +1,78 @@
 var express = require('express');
 var express_graphql = require('express-graphql');
 var {buildSchema} = require('graphql');
+// var query = require('./sql.js');
+const { Client }  = require('pg');
+const client = new Client({
+    user: 'codeboxx',
+    host: 'localhost',
+    database: 'postgres',
+    password: 'Bobek',
+    port: 5432
+});
 
+client.connect(function(error){
+    if (!!error) {
+        console.log("Unable to connect to PSQL database.")
+    } else {
+        console.log("You are connected to PSQL database.")
+    }
+});
+
+var mysql = require('mysql');
+const con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "Rocket_Elevators_Information_System_development"
+});
+
+con.connect(function(error){
+    if (!!error) {
+        console.log("Unable to connect to mySQL database.");
+    } else {
+        console.log("You are connected to mySQL database.");
+    }
+});
 // GraphQL Schema
 var schema = buildSchema(`
+    type Employee {
+        id: Int!
+        firstname: String
+        lastname: String
+        email: String
+        function: String
+    }
+    type Intervention {
+        id: Int!
+    }
     type Query {
         message: String
+        employees(id: Int!): Employee
     }
 `);
 
 // Root Resolver
 var root = {
-    message: () => 'Hello World!'
+    message: () => 'Hello World!',
+    employees: getEmployees
+};
+
+function query(queryString) {
+    console.log(queryString)
+    return new Promise((resolve, reject) => {
+        con.query(queryString, function(err, result) {
+            if (err) {
+                return reject(err);
+            } 
+            return resolve(result)
+        })
+    })
+}
+
+async function getEmployees({id}) {
+    var employees = await query('SELECT * FROM employees WHERE id = ' +id )
+    return employees[0]
 };
 
 // Create an express server and a GraphQL endpoint
